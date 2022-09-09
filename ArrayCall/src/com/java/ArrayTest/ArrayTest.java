da367cd4ea38b489c84ca1bab4a6e1d5c3750728
People-09 <vani.karri@peopletech.com>
2022-09-09 07:04:25 -0700

    code

---
+++ ArrayCall/src/com/java/ArrayTest/ArrayTest.java
@@ -0,0 +1,20 @@
+package com.java.ArrayTest;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+public static void main(String[] args) {
+	int  a[]=new int[5];
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the values");
+	for(int i=0;i<5;i++) {
+		a[i]=sc.nextInt();
+		
+	}
+	System.out.println("output");
+	for(int i=0;i<5;i++)
+	{
+		System.out.println(a[i]);
+	}
+}
+}
